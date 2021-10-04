1310708edb491a088199ad683c32afcf7a687f8a
Maks Zorin <maks.zorin@x5.ru>
2021-10-04 21:39:52 +0300

    added anthill

--- ant_game.py
+++ ant_game.py
@@ -5,9 +5,10 @@ import random
 pygame.init()
 
 SIZE_SCREEN = {'x': 400, 'y': 400}
-black = [0, 0, 0]
-white = [255, 255, 255]
-green = [0, 255, 0]
+BLACK = [0, 0, 0]
+WHITE = [255, 255, 255]
+GREEN = [0, 255, 0]
+BROWN = [128, 0, 0]
 game_exit = False
 
 class BaseGameObject:
@@ -21,11 +22,22 @@ class BaseGameObject:
     def render(self):
         pygame.draw.rect(self.screen, self.color, [self.x, self.y, self.size, self.size])
 
+
 class Leaf(BaseGameObject):
     @property
     def generate_new_coords(self):
         self.x = random.randint(1, SIZE_SCREEN.get('x') - self.size)
         self.y = random.randint(1, SIZE_SCREEN.get('y') - self.size)
+
+
+class AntHill(BaseGameObject):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.leaf_counter = 0
+
+    def leaf_counter(self):
+        self.leaf_counter += 1
+
 
 class Ant(BaseGameObject):
     def run(self, leaf_x, leaf_y, speed):
@@ -46,23 +58,26 @@ def intersect(x1, y1, size1, x2, y2, size2):
 
 screen = pygame.display.set_mode([SIZE_SCREEN.get('x'), SIZE_SCREEN.get('y')])
 
-ant = Ant(50, 50, 10, screen, black)
+ant = Ant(50, 50, 10, screen, BLACK)
 leaf_x = random.randint(1, 400)
 leaf_y = random.randint(1, 400)
-leaf = Leaf(leaf_x, leaf_y, 10, screen, green)
+leaf = Leaf(leaf_x, leaf_y, 10, screen, GREEN)
+anthill = AntHill(random.randint(1, 400), random.randint(1, 400), 20, screen, BROWN)
 
 while not game_exit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_exit = True
 
-    screen.fill(white)
+    screen.fill(WHITE)
 
     if intersect(ant.x, ant.y, ant.size, leaf.x, leaf.y, leaf.size):
         leaf.generate_new_coords
 
     ant.render()
     leaf.render()
+    anthill.render()
+
     ant.run(leaf.x, leaf.y, 10)
     print(f'ant coords:{ant.x} {ant.y}')
     print(f'leaf coords:{leaf.x} {leaf.y}')
